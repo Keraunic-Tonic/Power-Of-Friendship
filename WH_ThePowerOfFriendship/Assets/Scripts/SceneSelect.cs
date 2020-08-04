c5a5795b43ffc01f356657357471e300084d344c
Lauren Taylor <keraunic.tonic@gmail.com>
2020-08-04 12:41:50 -0400

    Added functionality to scene select

--- WH_ThePowerOfFriendship/Assets/Scripts/SceneSelect.cs
+++ WH_ThePowerOfFriendship/Assets/Scripts/SceneSelect.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
 
 public class SceneSelect : MonoBehaviour
 {
 
     public void ChooseScene()
     {
-        SceneManager.LoadScene("StartMenu");
+        if (EventSystem.current.currentSelectedGameObject.name == "AidaNeoButton")
+        {
+            SceneManager.LoadScene("AidaNeoScene");
+        }
+        else if (EventSystem.current.currentSelectedGameObject.name == "BlazeEganButton")
+        {
+            SceneManager.LoadScene("BlazeEganScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("DahliaRayScene");
+        }
     }
 }
